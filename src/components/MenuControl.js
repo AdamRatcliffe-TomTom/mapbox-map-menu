f5fdd63b0857cdd843abbcad8d02700b602f897f
Adam Ratcliffe <adam.ratcliffe@tomtom.com>
2023-09-28 11:39:29 -0700

    Add minimize button to MenuControl

--- src/components/MenuControl.js
+++ src/components/MenuControl.js
@@ -1,11 +1,22 @@
 import Menu from "./Menu";
+import createElement from "../functions/createElement";
 
 class MenuControl extends Menu {
   onAdd(map) {
     this.render(map);
-    this.element.classList.add("mapboxgl-ctrl");
+    this.element.classList.add("map-menu-control", "mapboxgl-ctrl");
+
+    this.element.appendChild(this.renderMinimizeButton());
 
     return this.element;
+  }
+
+  renderMinimizeButton() {
+    const buttonElement = createElement({
+      tagName: "button",
+      className: "map-menu-control-minimize-button"
+    });
+    return buttonElement;
   }
 
   onRemove() {
